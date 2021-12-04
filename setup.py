e43a34645c2bb723ef1a94b36ba6a8cdb1ad7dda
Henry Lao <hclao1995@gmail.com>
2021-12-03 17:10:50 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     # defined as the cli package (Note the naming)
-    name="mscgnet",
+    name="mscgNetV2",
     version="0.1.0",
     author="",
     author_email="",
